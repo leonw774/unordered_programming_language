c5cc4353b6a5ae39499c18bd6ea940df4fd3d356
leonw774 <leonw774@gmail.com>
2025-09-09 21:40:51 +0800

    use big single char alphbet & add debug flag

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
 import argparse
 import random
-import string
 import tokenize
 
 from collections import Counter
-from itertools import product, zip_longest
 
 from pyprimesieve import primes_nth
 
-# get all non-whitespace printable chars in a "messy" order
-NON_WS_PRINTABLES = [
-    c
-    for cs in zip_longest(
-        string.ascii_letters, reversed(string.digits), string.punctuation
-    )
-    for c in cs if c is not None
-]
-# use the product of our "messy" printables, also in "messy" order
-CHAR_PRODUCT = [l + r for l, r in product(NON_WS_PRINTABLES, repeat=2)]
-ALPHABET = [
-    p
-    for ps in zip_longest(
-        CHAR_PRODUCT[:2048], CHAR_PRODUCT[2048:4096], CHAR_PRODUCT[4096:6144],
-        CHAR_PRODUCT[6144:]
-    )
-    for p in ps if p is not None
-]
+# use non-whitespace printables
+GREEK_EXCLUDE = {
+    0x378, 0x379, 0x380, 0x381, 0x382, 0x383, 0x38b, 0x38d, 0x3a2
+}
+CYRILLIC_EXCLUDE = {
+    0x483, 0x484, 0x485, 0x486, 0x487, 0x488, 0x489
+}
+ARMENIAN_EXCLUDE = {
+    0x530, 0x557, 0x558, 0x58b, 0x58c
+}
+GREEK_EXTEND_EXCLUDE = {
+    0x1f16, 0x1f17, 0x1f1e, 0x1f1f, 0x1f46, 0x1f47, 0x1f4e, 0x1f4f, 0x1f58,
+    0x1f5a, 0x1f5c, 0x1f5e, 0x1f7e, 0x1f7f, 0x1fb5, 0x1fc5, 0x1fd4, 0x1fd5,
+    0x1fdc, 0x1ff0, 0x1ff1, 0x1ff5, 0x1fff
+}
+ALPHABET = (
+    # ASCII
+    [chr(u) for u in range(0x21, 0x7f)]
+    # Latin-1 Supplement
+    + [chr(u) for u in range(0xa1, 0x100) if u != 0xad]
+    # Latin Extended-A
+    + [chr(u) for u in range(0x100, 0x180)]
+    # Latin Extended-B
+    + [chr(u) for u in range(0x180, 0x250)]
+    # IPA Extensions
+    + [chr(u) for u in range(0x250, 0x2af)]
+    # Greek
+    + [chr(u) for u in range(0x370, 0x400) if u not in GREEK_EXCLUDE]
+    # Cyrillic
+    + [chr(u) for u in range(0x400, 0x500) if u not in CYRILLIC_EXCLUDE]
+    # Cyrillic Supplement
+    + [chr(u) for u in range(0x500, 0x530)]
+    # Armenian
+    + [chr(u) for u in range(0x530, 0x590) if u not in ARMENIAN_EXCLUDE]
+    # Latin Extended Additional
+    + [chr(u) for u in range(0x1e00, 0x1f00)]
+    # Greek Extended
+    + [chr(u) for u in range(0x1f00, 0x1fff) if u not in GREEK_EXTEND_EXCLUDE]
+    # Currency Symbols
+    + [chr(u) for u in range(0x20a0, 0x20c1)]
+    # Letterlike Symbols
+    + [chr(u) for u in range(0x2100, 0x2150)]
+    # Number Forms
+    + [chr(u) for u in range(0x2150, 0x218c)]
+    # Arrows & Mathematical Operators
+    + [chr(u) for u in range(0x2190, 0x2300)]
+    # Miscellaneous Technical
+    + [chr(u) for u in range(0x2300, 0x2400)]
+)
 VEC_SIZE = len(ALPHABET)
 
 def u4s_to_vec(xs: list[int]):
@@ -119,41 +148,54 @@ def encode(token_infos: list[tokenize.TokenInfo]) -> str:
             cur_indent_depth -= 1
         else:
             mini_token_infos.append((token_info.type, token_info.string))
-    # print(mini_token_infos)
+    if debug_flag:
+        print('tokens:', mini_token_infos)
     token_u4s = [
         n
         for ttype, tstr in mini_token_infos
         for n in token_info_to_u4s(ttype, tstr)
     ]
-    # print(token_u4s)
+    if debug_flag:
+        print('u4s:', token_u4s)
     vec = u4s_to_vec(token_u4s)
-    # print(vec)
-    # generate pairs accordding to vec
+    if debug_flag:
+        print('vector:', [n for n in vec if n != 1])
+    chars_count = sum(n - 1 for n in vec)
+    if debug_flag:
+        print('char#:', chars_count)
+    if chars_count > 1000_000:
+        print('result too large: output the vector instead')
+        return repr(vec)
+    # generate chars according to vec
     char_indices = [
         i
         for i, n in enumerate(vec) if n != 1
         for _ in range(n - 1)
     ]
-    # print(len(char_indices))
-    if len(char_indices) > 100_000: # 200 MB
-        print("result too large. program ends without output.")
-        exit()
-    random.shuffle(char_indices)
+    if chars_count > 100_000:
+        print('result too large. output without shuffling the characters.')
+    else:
+        random.shuffle(char_indices)
     return ''.join(ALPHABET[i] for i in char_indices)
 
 def decode(chars: str) -> list[tokenize.TokenInfo]:
     # count occurrences of each ALPHABET element
     counts = Counter([chars[i:i+2] for i in range(0, len(chars), 2)])
     vec = [counts.get(a, 0) + 1 for a in ALPHABET]
-    # print(vec)
+    if debug_flag:
+        print('vector:', [n for n in vec if n != 1])
     token_u4s = vec_to_u4s(vec)
-    # print(token_u4s)
+    if debug_flag:
+        print('u4s:', token_u4s)
     mini_token_infos = u4s_to_token_infos(token_u4s)
-    # print(mini_token_infos)
+    if debug_flag:
+        print('tokens:', mini_token_infos)
     return mini_token_infos
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+
     subparser = parser.add_subparsers(dest='command')
     enc_parser = subparser.add_parser('enc')
     enc_parser.add_argument(
@@ -182,8 +224,15 @@ def parse_args():
     )
     return parser.parse_args()
 
+debug_flag = False
+
 def main():
     args = parse_args()
+    global debug_flag
+    debug_flag = args.debug
+    if debug_flag:
+        print('alphabet count:', len(ALPHABET))
+        print('alphabet:', ''.join(ALPHABET))
     mode = args.command
     if mode == 'enc':
         # read the input python content
@@ -205,5 +254,5 @@ def main():
             chars = f.read()
         exec(tokenize.untokenize(decode(chars)))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
